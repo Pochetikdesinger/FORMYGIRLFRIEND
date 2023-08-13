2a4d45fd315ddac7d6fa22029206224a3d43a72d
Pool Dev <86147930+Pochetikdesinger@users.noreply.github.com>
2023-08-13 00:16:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('mouseenter', () => {
+        card.classList.add('hover');
+    });
+
+    card.addEventListener('mouseleave', () => {
+        card.classList.remove('hover');
+    });
+});
